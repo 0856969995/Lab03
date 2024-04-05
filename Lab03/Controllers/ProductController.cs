6c22a2b69507deaac8e2a07777ab96335734c4a2
hoang <hoang@ADMIN>
2024-04-05 23:49:51 +0700

    Add project files.

---
+++ Lab03/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Lab03.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab03.Controllers
+{
+    public class ProductController: Controller
+    {
+        private readonly IProductRepository _productRepository; 
+        private readonly ICategoryRepository _categoryRepository;
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return View(products);
+        }
+
+    }
+}
